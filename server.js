702dc13565093c19dffd814788da2f4aace57a79
Mrityunjay Barman <mrityunjaybarman@Mrityunjays-MacBook-Air.local>
2024-12-29 20:40:32 +0530

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB_URL = process.env.DATABASE_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+}).then((con) => {
+  console.log('Connected');
+})
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    reqired: [true, 'A tour must have a name']
+  }, 
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
